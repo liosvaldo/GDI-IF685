614fef7fbc43f908639a2ace237457992d38a527
Lukas Asael <43383069+lukasasael@users.noreply.github.com>
2023-12-19 22:53:39 -0300

    creating tables

--- sql_create_table.sql
+++ sql_create_table.sql
@@ -1,6 +1,23 @@
 CREATE TABLE cep ( 
 	cep 					NUMBER 					NOT NULL,
-	rua 					VARCHAR2(6)				NOT NULL, 
-	cidade 					VARCHAR2(6)				NOT NULL, 
+	rua 					VARCHAR2(6)						, 
+	cidade 					VARCHAR2(6)						, 
 	CONSTRAINT cep_pk 		PRIMARY KEY (cep)
+);
+
+CREATE TABLE modeloeletrocentro (
+	modelo					NUMBER					NOT NULL,
+	fabricante				VARCHAR(6)						,
+	pot_nom					NUMBER							,
+	tensao_nominal			NUMBER							,
+	autonomia_baterias		NUMBER							,
+	corrente_max_baterias	NUMBER							
+);
+
+CREATE TABLE secoletora (
+	id 						NUMBER					NOT NULL,
+	nome_parque				VARCHAR2(6)						,
+	id_ponto_conexao		NUMBER					NOT NULL,
+	pot_nom					NUMBER							,
+	tensao_nom				NUMBER					
 );
