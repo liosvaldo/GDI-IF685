c65586825c4d07a0f57d5400fb0665a03b13a7b6
BRVIS863124 <BRVIS863124@ambev.com.br>
2023-12-21 17:49:21 -0300

    fit: upgrade e testes sql_create_table.sql
    Atualizamos o script sql_create_table.sql como
    também realizamos os testes em ambiente de
    Sql live oracle .

--- sql_create_table.sql
+++ sql_create_table.sql
@@ -1,5 +1,16 @@
+DROP TABLE operador;
+DROP TABLE cep;
+DROP TABLE modelo_inversor;
+DROP TABLE modelo_painel;
+DROP TABLE modelo_eletrocentro;
+DROP TABLE conjunto;
+DROP TABLE instalacao;
+DROP TABLE eletrocentro;
+DROP TABLE se_coletora;
+DROP TABLE ponto_conexao;
+
 CREATE TABLE operador(
-    matricula               varchar2(10)                    ,
+    matricula               number                    ,
     nome                    varchar2(60)            NOT NULL,
     cpf                     varchar2(14)            NOT NULL,
     rua                     varchar2(30)            NOT NULL,
@@ -12,19 +23,85 @@ CREATE TABLE operador(
     CONSTRAINT  cep_ck                              CHECK           (cep LIKE '_____-___')
 );
 
-CREATE TABLE instalacao(
+CREATE TABLE cep(
+
+	cep 					NUMBER 					NOT NULL,
+	rua 					VARCHAR2(6)						,
+	cidade 					VARCHAR2(6)						,
+	CONSTRAINT cep_pk 		PRIMARY KEY (cep),
+	CONSTRAINT cep_fk       CHECK (cep LIKE '______-___)')
+
+);
+
+CREATE TABLE modelo_inversor(
+
+    modelo                  varchar2(20)                    ,
+    fabricante              varchar2(30)                    ,
+    tensao_maxima_cc        number                  NOT NULL,
+    pot_max_cc              number                  NOT NULL,
+    pot_max_ca              number                  NOT NULL,
+    pot_nom_ca              number                  NOT NULL,
+    num_entradas_cc         number                  NOT NULL,
+    CONSTRAINT modelo_inversor_pk                 PRIMARY KEY     (modelo)
+);
+
+CREATE TABLE modelo_painel(
+
+    modelo                  varchar2(20)                    ,
+    fabricante              varchar2(30)                    ,
+    corrente_pico           number                  NOT NULL,
+    tensao_max_func         number                  NOT NULL,
+    pot_max                 number                  NOT NULL,
+    temp_max                number                  NOT NULL,
+    temp_min                number                  NOT NULL,
+    CONSTRAINT modelo_painel_pk                 PRIMARY KEY     (modelo)
+);
+
+CREATE TABLE modelo_eletrocentro(
+
+	modelo					NUMBER					        ,
+	fabricante				VARCHAR2(6)						,
+	pot_nom					NUMBER				    NOT NULL,
+	tensao_nom  			NUMBER					NOT NULL,
+	autonomia_baterias		NUMBER					NOT NULL,
+	corrente_max_baterias	NUMBER                  NOT NULL,
+	CONSTRAINT modelo_eletrocentro_pk                            PRIMARY KEY     (modelo)
+
+);
+
+CREATE TABLE conjunto(
     id                      number                          ,
-    id_conjunto             number                  NOT NULL,
-    id_operador             number                  NOT NULL,
-    id_supervisor           number                  NOT NULL,
-    id_eletrocentro         number                  NOT NULL,
+    modelo_inversor         varchar2(30)                    ,
+    modelo_painel           varchar2(30)                    ,
+    CONSTRAINT id_conjutno_pk                               PRIMARY KEY     (id),
+    CONSTRAINT modelo_inversor_fk                   FOREIGN KEY     (modelo_inversor) REFERENCES modelo_inversor(modelo),
+    CONSTRAINT modelo_painel_fk                     FOREIGN KEY     (modelo_painel)  REFERENCES modelo_painel(modelo)
+);
+
+CREATE TABLE ponto_conexao(
+    id                      number                          ,
+	nome    				VARCHAR2(20)	        NOT NULL,
+    pot_nom                 NUMBER                  NOT NULL,
+    estado                  VARCHAR2(20)            NOT NULL,
+    cidade                  VARCHAR2(20)            NOT NULL,
     latitude                number                  NOT NULL,
     longitude               number                  NOT NULL,
-    CONSTRAINT  id_pk                               PRIMARY KEY     (id),
-    CONSTRAINT  id_conjunto_fk                      FOREIGN KEY     (id_conjunto)       REFERENCES  conjunto(id),
-    CONSTRAINT  id_operador_fk                      FOREIGN KEY     (id_operator)       REFERENCES  operador(id),
-    CONSTRAINT  id_supervisor_fk                    FOREIGN KEY     (id_supervisor)     REFERENCES  operador(id),
-    CONSTRAINT  id_eletrocentro_fk                  FOREIGN KEY     (id_eletrocentro)   REFERENCES  eletrocentro(id)
+    CONSTRAINT  id_ponto_conexao_pk                               PRIMARY KEY     (id),
+    CONSTRAINT  estado_ponto_conexao_ck                           CHECK   (estado IN ('BH', 'MG', 'AL'))
+);
+
+CREATE TABLE se_coletora(
+	id 						NUMBER					NOT NULL,
+	nome_parque				VARCHAR2(20)	        NOT NULL,
+	id_ponto_conexao		NUMBER					NOT NULL,
+	pot_nom					NUMBER					NOT NULL,
+	tensao_nom				NUMBER                  NOT NULL,
+	taxa_conversao          NUMBER                  NOT NULL,
+	latitude                number                  NOT NULL,
+    longitude               number                  NOT NULL,
+    CONSTRAINT  id_se_coletora_pk                               PRIMARY KEY     (id),
+    CONSTRAINT  id_ponto_conexao_fk                 FOREIGN KEY     (id_ponto_conexao)          REFERENCES  ponto_conexao(id)
+
 );
 
 CREATE TABLE eletrocentro(
@@ -35,50 +112,29 @@ CREATE TABLE eletrocentro(
     id_coletora             number                  NOT NULL,
     latitude                number                  NOT NULL,
     longitude               number                  NOT NULL,
-    CONSTRAINT  id_pk                               PRIMARY KEY     (id),
-    CONSTRAINT  id_modelo_eletrocentro_fk           FOREIGN KEY     (id_modelo_eletrocentro)    REFERENCES  ModeloEletrocentro(id),
-    CONSTRAINT  id_operador_fk                      FOREIGN KEY     (id_operator)               REFERENCES  operador(id),
-    CONSTRAINT  id_supervisor_fk                    FOREIGN KEY     (id_supervisor)             REFERENCES  operador(id),
-    CONSTRAINT  id_eletrocentro_fk                  FOREIGN KEY     (id_eletrocentro)           REFERENCES  eletrocentro(id)
-
+    CONSTRAINT  id_eletrocentro_pk                  PRIMARY KEY     (id),
+    CONSTRAINT  id_modelo_eletrocentro_fk           FOREIGN KEY     (id_modelo_eletrocentro)    REFERENCES  modelo_eletrocentro(modelo),
+    CONSTRAINT  id_operador_eletrocentro_fk         FOREIGN KEY     (id_operador)               REFERENCES  operador(matricula),
+    CONSTRAINT  id_supervisor_eletrocentro_fk       FOREIGN KEY     (id_supervisor)             REFERENCES  operador(matricula),
+    CONSTRAINT  id_se_coletora_fk                   FOREIGN KEY     (id_coletora)               REFERENCES  se_coletora(id)
 );
 
-CREATE TABLE ponto_conexao(
+CREATE TABLE instalacao(
     id                      number                          ,
-    id_modelo_eletrocentro  number                  NOT NULL,
+    id_conjunto             number                  NOT NULL,
     id_operador             number                  NOT NULL,
     id_supervisor           number                  NOT NULL,
-    id_coletora             number                  NOT NULL,
+    id_eletrocentro         number                  NOT NULL,
     latitude                number                  NOT NULL,
     longitude               number                  NOT NULL,
-    CONSTRAINT  id_pk                               PRIMARY KEY     (id),
-    CONSTRAINT  id_modelo_eletrocentro_fk           FOREIGN KEY     (id_modelo_eletrocentro)    REFERENCES  ModeloEletrocentro(id),
-    CONSTRAINT  id_operador_fk                      FOREIGN KEY     (id_operator)               REFERENCES  operador(id),
-    CONSTRAINT  id_supervisor_fk                    FOREIGN KEY     (id_supervisor)             REFERENCES  operador(id),
-    CONSTRAINT  id_eletrocentro_fk                  FOREIGN KEY     (id_eletrocentro)           REFERENCES  eletrocentro(id)
-
+    CONSTRAINT  id_instalacao_pk                    PRIMARY KEY     (id),
+    CONSTRAINT  id_conjunto_fk                      FOREIGN KEY     (id_conjunto)       REFERENCES  conjunto(id),
+    CONSTRAINT  id_operador_instalacao_fk           FOREIGN KEY     (id_operador)       REFERENCES  operador(matricula),
+    CONSTRAINT  id_supervisor_instalacao_fk         FOREIGN KEY     (id_supervisor)     REFERENCES  operador(matricula),
+    CONSTRAINT  id_eletrocentro_fk                  FOREIGN KEY     (id_eletrocentro)   REFERENCES  eletrocentro(id)
 );
 
-CREATE TABLE cep ( 
-	cep 					NUMBER 					NOT NULL,
-	rua 					VARCHAR2(6)						, 
-	cidade 					VARCHAR2(6)						, 
-	CONSTRAINT cep_pk 		PRIMARY KEY (cep)
-);
 
-CREATE TABLE modeloeletrocentro (
-	modelo					NUMBER					NOT NULL,
-	fabricante				VARCHAR(6)						,
-	pot_nom					NUMBER							,
-	tensao_nominal			NUMBER							,
-	autonomia_baterias		NUMBER							,
-	corrente_max_baterias	NUMBER							
-);
 
-CREATE TABLE secoletora (
-	id 						NUMBER					NOT NULL,
-	nome_parque				VARCHAR2(6)						,
-	id_ponto_conexao		NUMBER					NOT NULL,
-	pot_nom					NUMBER							,
-	tensao_nom				NUMBER					
-);
+
+
