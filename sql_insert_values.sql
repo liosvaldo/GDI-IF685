a1b56b01fc9f74e63b72ec9b04dda0ee077e21cd
Paulo Carneiro <38364866+phjc@users.noreply.github.com>
2023-12-21 23:18:35 -0300

    Update sql_insert_values.sql

--- sql_insert_values.sql
+++ sql_insert_values.sql
@@ -1,1 +1,14 @@
-INSER VALUE SCRIPT
+--INSERINDO PACOTES PRODUTO ENERGIA
+
+INSERT INTO produto_energia (id_produto, prazo_de_entrega, quantidade_kw, data_de_venda) VALUES (0001, "12/01/24", 300, "12/12/23");
+INSERT INTO produto_energia (id_produto, prazo_de_entrega, quantidade_kw, data_de_venda) VALUES (0002, "12/02/24", 300, "12/01/24");
+INSERT INTO produto_energia (id_produto, prazo_de_entrega, quantidade_kw, data_de_venda) VALUES (0003, "02/01/24", 400, "02/12/23");
+INSERT INTO produto_energia (id_produto, prazo_de_entrega, quantidade_kw, data_de_venda) VALUES (0004, "02/02/24", 300, "02/01/24");
+
+
+--INSERINDO OPERADORES
+
+INSERT INTO operador (matricula, nome, cpf, rua, numero, complemento, cidade, cep) VALUES (0001, "José Valente", "123.456.789-00", "Rua Brazil", 333, "Recife", "53123-456");
+INSERT INTO operador (matricula, nome, cpf, rua, numero, complemento, cidade, cep) VALUES (0002, "Saulo Monela", "445.556.667-77", "Rua Outôno", 413, "Jaboatão", "53134-567");
+INSERT INTO operador (matricula, nome, cpf, rua, numero, complemento, cidade, cep) VALUES (0004, "Jacinto Sono", "987.543.109-62", "Avenida Silva", 003, "Caruaru", "53884-556");
+INSERT INTO operador (matricula, nome, cpf, rua, numero, complemento, cidade, cep) VALUES (0008, "Malvo Olhado", "776.455.903-24", "BR-101", 84, "Paulista", "53444-111");
