fd8a2a8e797ea9f156adc3449e069012f46994a8
Lucas Sales <lucas-francisco98@hotmail.com>
2024-02-27 17:01:28 -0300

    att
    reorganização do código e adicionei outras entidades e relações

--- sql_create_or.sql
+++ sql_create_or.sql
@@ -1,24 +1,8 @@
-/* Criação da base da entidade 
-pessoa,
-endereco, 
-localizacao, 
-identificaçãoSistema, 
-telefone, 
-pessoaFisica, 
-pessoaJuridica, 
-produtoEnergia, 
-medidorPontoDeConexao, 
-PontoConexao,
-tb_possuiGeracaoPontoDeConexao,
-tp_negocia,
-tp_lista_negociacoes,
-tb_lista_negociacoes,
- ainda precisa modificar*/
-
+--Criação de tipos:
 CREATE OR REPLACE TYPE tp_endereco AS OBJECT
 (
     cidade VARCHAR2(50),
-    estado VARCHAR2(2) -- Só permitido a sigla
+    estado CHAR(2) -- Só permitido siglas de estado
 );
 /
 
@@ -31,8 +15,8 @@ CREATE OR REPLACE TYPE tp_localizacao AS OBJECT
 
 CREATE OR REPLACE TYPE tp_id_sistema AS OBJECT
 (
-    nome_parque varchar2(40),
-    pot_nom number
+    nome_parque VARCHAR2(40),
+    pot_nom NUMBER
 );
 /
 
@@ -49,7 +33,7 @@ CREATE OR REPLACE TYPE tp_telefone AS OBJECT
     fone VARCHAR2(10)
 );
 /
-/*Aqui o array só permite 2 telefones diferentes*/
+
 CREATE TYPE lista_tp_fone AS VARRAY(2) OF tp_telefone;
 /
 
@@ -74,14 +58,10 @@ CREATE OR REPLACE TYPE BODY tp_pessoa AS
 END;
 /
 
-/* NOT INSTANTIABLE, pois por meio desses tipos não será gerado nenhuma tabela*/
 CREATE OR REPLACE TYPE tp_pessoaFisica UNDER tp_pessoa
 (
     cpf NUMBER
 ) FINAL;
-/
-
-CREATE TABLE tb_pessoaFisica OF tp_pessoaFisica;
 /
 
 CREATE OR REPLACE TYPE tp_pessoaJuridica UNDER tp_pessoa
@@ -89,15 +69,6 @@ CREATE OR REPLACE TYPE tp_pessoaJuridica UNDER tp_pessoa
     cnpj VARCHAR2(14),
     nome_fantasia VARCHAR2(20)
 ) FINAL;
-/
-
-CREATE TABLE tb_pessoaJuridica OF tp_pessoaJuridica;
-/
-
-CREATE TABLE tb_pessoa OF tp_pessoa
-(
-    id PRIMARY KEY
-);
 /
 
 CREATE OR REPLACE TYPE tp_produtoEnergia AS OBJECT
@@ -109,44 +80,71 @@ CREATE OR REPLACE TYPE tp_produtoEnergia AS OBJECT
 );
 /
 
-CREATE OR REPLACE TYPE tp_medidorPontoDeConexao AS OBJECT
-(
-    id_medidor NUMBER,
-    num_de_serie NUMBER,
-    num_prioridade NUMBER
-);
-/
-
-CREATE TABLE tb_possuiGeracaoPontoDeConexao OF tp_medidorPontoDeConexao;
-/
-    
 CREATE OR REPLACE TYPE tp_pontoConexao AS OBJECT
 (
     id NUMBER,
     endereco tp_endereco,
     localizacao tp_localizacao,
-    identificacao_sistema tp_identificacaoSistema,
-    possui_geracao_ponto_de_conexao tp_medidorPontoDeConexao
+    identificacao_sistema tp_identificacaoSistema
+);
+/
+
+CREATE OR REPLACE TYPE tp_eletrocentro AS OBJECT
+(
+    id_eletrocentro NUMBER,
+    localizacao tp_localizacao
+);
+/
+
+CREATE OR REPLACE TYPE tp_medidorPontoDeConexao AS OBJECT
+(
+    id_medidor NUMBER,
+    num_de_serie NUMBER,
+    num_prioridade NUMBER,
+    pontoConexao REF tp_pontoConexao
 );
 /
 
 CREATE OR REPLACE TYPE tp_seColetora AS OBJECT
 (
-    id number,
-    tensao_nom number,
-    taxa_de_conversao number,
-    localização tp_localizacao,
+    id NUMBER,
+    tensao_nom NUMBER,
+    taxa_de_conversao NUMBER,
+    localizacao tp_localizacao,
     id_sistema tp_id_sistema,
     seConecta REF tp_pontoConexao
 );
 /
 
-CREATE TABLE tb_seColetora OF tp_seColetora
+CREATE OR REPLACE TYPE tp_MedidorseColetora AS OBJECT
 (
-    PRIMARY KEY (id)
+    id_medidor NUMBER,
+    num_prioridade NUMBER,
+    num_serie NUMBER,
+    geracaoSeColetora REF tp_seColetora
 );
 /
-    
+
+CREATE OR REPLACE TYPE tp_medidorEletrocentro AS OBJECT
+(
+    id_medidor NUMBER,
+    num_serie NUMBER,
+    num_prioridade NUMBER,
+    eletrocentro REF tp_eletrocentro
+);
+/
+
+CREATE OR REPLACE TYPE tp_modeloEletrocentro AS OBJECT
+(
+    modelo VARCHAR2(40),
+    fabricante VARCHAR2(40),
+    tensao_nom NUMBER,
+    autonomia_baterias VARCHAR2(40),
+    corr_max_baterias NUMBER,
+    pont_nom NUMBER
+);
+/
+
 CREATE OR REPLACE TYPE tp_negocia AS OBJECT
 (
     idNegociacao NUMBER,
@@ -159,10 +157,62 @@ CREATE OR REPLACE TYPE tp_negocia AS OBJECT
 CREATE TYPE tp_lista_negociacoes AS TABLE OF tp_negocia;
 /
 
+CREATE TYPE tp_lista_medidorPontoDeConexao AS TABLE OF tp_medidorPontoDeConexao;
+/
+
+CREATE TYPE tp_lista_medidorEletrocentro AS TABLE OF tp_medidorEletrocentro;
+/
+
+CREATE TYPE tp_lista_MedidorseColetora AS TABLE OF tp_MedidorseColetora;
+/
+
+--Criando tabelas:
+
+CREATE TABLE tb_pessoaFisica OF tp_pessoaFisica;
+/
+
+CREATE TABLE tb_pessoaJuridica OF tp_pessoaJuridica;
+/
+
+CREATE TABLE tb_pessoa OF tp_pessoa
+(
+    id PRIMARY KEY
+);
+/
+
+CREATE TABLE tb_possuiGeracaoPontoDeConexao
+(
+    timestamp_geracao DATE,
+    potencia NUMBER,
+    GeracaoPontoConexao tp_lista_medidorPontoDeConexao
+) NESTED TABLE GeracaoPontoConexao STORE AS tb_pontoConexao;
+/
+    
+CREATE TABLE tb_seColetora OF tp_seColetora
+(
+    PRIMARY KEY (id)
+);
+/
+
+CREATE TABLE tb_geracaoEletrocentro
+(
+    potencia NUMBER,
+    timestamp_geracao DATE,
+    medidorEletrocentro tp_lista_medidorEletrocentro
+) NESTED TABLE medidorEletrocentro STORE AS tb_medidorEletrocentro;
+/
+
+CREATE TABLE tb_geracaoSEColetora
+(
+    potencia NUMBER,
+    timestamp_medidor DATE,
+    geracaoMedidor tp_lista_MedidorseColetora
+) NESTED TABLE geracaoMedidor STORE AS tb_medidorseColetora;
+/
+
 CREATE TABLE tb_lista_negociacoes 
 (
     idNegociacao NUMBER PRIMARY KEY,
     negociacoes tp_lista_negociacoes
-) NESTED TABLE negociacoes STORE AS tb_negociacao_negociacoes;
+) NESTED TABLE negociacoes STORE AS tb_negociacoes;
 /
-
